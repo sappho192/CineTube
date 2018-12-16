32c14c95e4a38690d921f6cb388612abbcd55762
sappho192 <sappho192@gmail.com>
2018-12-17 03:37:36 +0900

    회원가입 기능구현까지 다했는데 SQL이 먹히질 않어

--- aspnetproj/Cinetube/Controllers/HomeController.cs
+++ aspnetproj/Cinetube/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Cinetube.Models;
@@ -91,6 +92,31 @@ namespace Cinetube.Controllers
         public IActionResult Logout()
         {
             session.Clear();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult SignUp(string ID, string PW, string name, string birth, string ssn, string phone, int PWHintNo, string PWAns)
+        {
+            if (ID == null || PW == null || name == null || birth == null || ssn == null || phone == null || PWAns == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            if (GlobalVariables.PhoneRegex.Match(phone).Success)
+            {
+                // 010 3942 3438
+                phone = phone.Insert(7, "-").Insert(3, "-");
+            }
+
+            using (var connection = new SqlConnection("server = sappho192.iptime.org;database = CinetubeDB2;uid=cinetube;pwd=qwer12#$;"))
+            {
+                string commandStr =
+                    $"DECLARE @NUM INT\r\nSET @NUM = (SELECT COUNT(*) FROM 사용자)\r\nIF (@NUM != 0) SET @NUM = (SELECT MAX(사용자번호) FROM 사용자) + 1\r\n\r\nINSERT INTO 사용자 VALUES(\r\n@NUM,\r\n\'{ID}\',\r\n\'{PW}\',\r\n\'{name}\',\r\n\'{birth}\',\r\n{ssn},\r\n\'{phone}\'\r\n);\r\n\r\nINSERT INTO 회원 VALUES(\r\n@NUM,\r\n0,\r\n{PWHintNo},\r\n\'{PWAns}\'\r\n);";
+                var command = new SqlCommand(commandStr, connection);
+                connection.Open();
+                Console.WriteLine($"ID: {ID}, PW: {PW}, name: {name}, birth: {birth}, 주민번호: {ssn}, phone: {phone}, PWHintNo: {PWHintNo}, PWAns: {PWAns}");
+            }
 
             return RedirectToAction("Index", "Home");
         }
