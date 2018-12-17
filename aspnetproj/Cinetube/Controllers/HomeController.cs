00a8644a81c97642893508edd9013ee8068e7243
sappho192 <sappho192@gmail.com>
2018-12-17 21:19:44 +0900

    내 정보에 구매/충전 내역 추가

--- aspnetproj/Cinetube/Controllers/HomeController.cs
+++ aspnetproj/Cinetube/Controllers/HomeController.cs
@@ -1,10 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Diagnostics;
-using System.Linq;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Cinetube.Models;
 using Microsoft.AspNetCore.Http;
@@ -118,6 +116,57 @@ namespace Cinetube.Controllers
 
         public IActionResult MyPage()
         {
+            string ID = String.Empty;
+            foreach (String key in session.Keys)
+            {
+                if (key.Equals("Loggedin") && session.GetString(key) == "true")
+                {
+                    ID = session.GetString("ID");
+                }
+            }
+
+            using (var connection = new SqlConnection(GlobalVariables.connectionUrl))
+            {
+                string 구매내역 =
+                    $"DECLARE @MOVIENUM INT\r\nDECLARE @USERNUM INT\r\nSET @USERNUM = (SELECT 사용자번호 FROM 사용자 WHERE ID = \'{ID}\')\r\nSELECT 제목,영화.영화번호,구매번호 ,구매시각 ,만료일자 FROM 구매내역\r\n  INNER JOIN 영화 ON 구매내역.영화번호 = 영화.영화번호";
+                string 충전내역 = $"SELECT 충전금액, 충전시각 FROM 충전내역\r\n  WHERE 사용자번호 = (SELECT 사용자번호 FROM 사용자 WHERE ID = \'{ID}\')";
+                var commandPurchased = new SqlCommand(구매내역, connection);
+                var commandCharged = new SqlCommand(충전내역, connection);
+                connection.Open();
+
+                // 영화번호, 구매번호, 구매시각, 만료일자
+                List<PurchaseHistory> purchaseHistories = new List<PurchaseHistory>();
+                using (var reader = commandPurchased.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string 영화제목 = reader[0] is DBNull ? string.Empty : Convert.ToString(reader[0]);
+                        int 영화번호 = reader[0] is DBNull ? 0 : Convert.ToInt32(reader[1]);
+                        int 구매번호 = reader[1] is DBNull ? 0 : Convert.ToInt32(reader[2]);
+                        string 구매시각 = reader[2] is DBNull ? String.Empty : Convert.ToString(reader[3]);
+                        string 만료일자 = reader[3] is DBNull ? String.Empty : Convert.ToString(reader[4]);
+                        purchaseHistories.Add(new PurchaseHistory(영화제목, 영화번호, 구매번호, 구매시각, 만료일자));
+                        Console.WriteLine($"영화제목: {영화제목}, 영화번호: {영화번호}, 구매번호: {구매번호}, 구매시각: {구매시각}, 만료일자: {만료일자}");
+                    }
+                }
+
+                List<ChargeHistory> chargeHistories = new List<ChargeHistory>();
+                using (var reader = commandCharged.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int 금액 = reader[0] is DBNull ? 0 : Convert.ToInt32(reader[0]);
+                        string 충전시각 = reader[0] is DBNull ? string.Empty : Convert.ToString(reader[1]);
+                        chargeHistories.Add(new ChargeHistory(금액, 충전시각));
+                        Console.WriteLine($"충전 금액: {금액}, 충전시각: {충전시각}");
+                    }
+                }
+
+                ViewData["PurchaseHistories"] = purchaseHistories;
+                ViewData["ChargeHistories"] = chargeHistories;
+            }
+
+            //ViewData["ChargeHistory"] = 
             return View();
         }
 
@@ -169,7 +218,7 @@ namespace Cinetube.Controllers
                 var command = new SqlCommand(commandStr, connection);
                 connection.Open();
                 var result = command.ExecuteNonQuery();
-                return RedirectToAction("Charge", "Home", new {result = "SUCCESS"});
+                return RedirectToAction("Charge", "Home", new { result = "SUCCESS" });
             }
         }
     }
---
+++ aspnetproj/Cinetube/Models/PurchaseHistory.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cinetube.Models
+{
+    public class PurchaseHistory
+    {
+        public PurchaseHistory(string moviename, int movie, int purchased, string purchasedTime, string expiryTime)
+        {
+            영화제목 = moviename;
+            영화번호 = movie;
+            구매번호 = purchased;
+            구매시각 = purchasedTime;
+            만료일자 = expiryTime;
+        }
+
+        public string 영화제목 { get; }
+        public int 영화번호 { get; }
+        public int 구매번호 { get; }
+        public string 구매시각 { get; }
+        public string 만료일자 { get; }
+    }
+}
