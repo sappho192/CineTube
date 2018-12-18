456b283e173ff1f75483219a1f4df0f7f9958ecb
sappho192 <sappho192@gmail.com>
2018-12-18 23:47:38 +0900

    메인페이지에서도 결제 제대로 되게 해결, 로그인 로직 강화, doCharge 리다이렉트기능 보완

---
+++ aspnetproj/Cinetube/Models/RecentArticle.cs
@@ -0,0 +1,16 @@
+﻿namespace Cinetube.Models
+{
+    public class RecentArticle
+    {
+        public RecentArticle(string id, string title, int articleNo)
+        {
+            ID = id;
+            제목 = title;
+            글번호 = articleNo;
+        }
+
+        public string ID { get; }
+        public string 제목 { get; }
+        public int 글번호 { get; }
+    }
+}
