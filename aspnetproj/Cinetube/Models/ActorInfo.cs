b6cea3213b5c723da7b64e560020a9a6b1678ed4
sappho192 <sappho192@gmail.com>
2018-12-20 02:42:21 +0900

    영화 세부정보에 배우 정보 표시 & 배우 검색 구현완료

---
+++ aspnetproj/Cinetube/Models/ActorInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace Cinetube.Models
+{
+    public class ActorInfo
+    {
+        public ActorInfo(string name, int actorNo)
+        {
+            이름 = name;
+            배우번호 = actorNo;
+        }
+
+        public string 이름 { get; }
+        public int 배우번호 { get; }
+    }
+}
